a96fe1d01aabd55d51e5fe55204510897a85f75b
longrui <18813867725@163.com>
2020-06-08 10:10:34 +0800

    matplotlib

---
+++ Chpter9/matplot_06.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import  numpy as np
+import  matplotlib.pyplot as plt
+
+###注释与子图加工 text,arrow,annote
+import pandas as pd
+from numpy.random import randn
+
+from datetime import datetime
+fig = plt.figure()
+ax = fig.add_subplot(111)
+data = pd.read_csv
+
+
+
--- Chpter9/折线图.py
+++ Chpter9/折线图.py
@@ -1,8 +1,15 @@
 # -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+#x = np.linspace(-10,10,9)
+#y = x**2-2*x+5
+#plt.plot(x,y)
 
-x = np.linspace(-10,10,9)
-y = x**2-2*x+5
-plt.plot(x,y)
+###用pandas绘图
+s = pd.Series(np.random.randn(10).cumsum(),index=np.arange(0,100,10))
+s.plot()
+
+df = pd.DataFrame(np.random.randn(10,4).cumsum(0),columns=['A','B','C','D'],index=np.arange(0,100,10))
+df.plot()
 plt.show()
